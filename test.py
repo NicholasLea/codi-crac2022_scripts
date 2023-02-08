1706c2be5e055decea19eb1089ce2e4d6d742594
NicholasLea <598764500@qq.com>
2023-02-08 20:10:56 +0800

    mac

--- test.py
+++ test.py
@@ -9,11 +9,20 @@ import helper
 
 # 对于Bridging 和 Discourse Deixis，后面再说。我本次可能只是对Identity Anaphora进行微调
 
-helper.convert_coref_ua_to_json('D:\Dataset\LIGHT\light_train.2022.CONLLUA', \
-                                'D:\Dataset\LIGHT\light_train.2022.jsonlines', \
-                                MODEL="coref-hoi", SEGMENT_SIZE=512, TOKENIZER_NAME="spanbert-base-cased")
+# helper.convert_coref_ua_to_json('D:\Dataset\LIGHT\light_train.2022.CONLLUA', \
+#                                 'D:\Dataset\LIGHT\light_train.2022.jsonlines', \
+#                                 MODEL="coref-hoi", SEGMENT_SIZE=512, TOKENIZER_NAME="spanbert-base-cased")
 
 
 # helper.convert_coref_ua_to_json('D:\Dataset\LIGHT\light_dev.2022.CONLLUA', \
 #                                 'D:\Dataset\LIGHT\light_dev.2022.jsonlines', \
 #                                 MODEL="coref-hoi", SEGMENT_SIZE=512, TOKENIZER_NAME="spanbert-base-cased")
+
+# mac代码
+# helper.convert_coref_ua_to_json('/Users/nicholas/Documents/Dataset/LIGHT/light_dev.2022.CONLLUA', \
+#                                 '/Users/nicholas/Documents/Dataset/LIGHT/light_train.2022.bert-base-cased.jsonlines', \
+#                                 MODEL="coref-hoi", SEGMENT_SIZE=512, TOKENIZER_NAME="bert-base-cased")
+
+helper.convert_coref_ua_to_json('/Users/nicholas/Documents/Dataset/LIGHT/light_dev.2022.CONLLUA', \
+                                '/Users/nicholas/Documents/Dataset/LIGHT/light_train.2022.bert-base-cased.jsonlines', \
+                                MODEL="coref-hoi", SEGMENT_SIZE=512, TOKENIZER_NAME="bert-base-cased")
