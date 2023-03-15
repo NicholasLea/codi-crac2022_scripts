263d4911d684f5104387901d174b3792a2544278
NicholasLea <598764500@qq.com>
2023-03-15 23:09:31 +0800

    mac

--- test.py
+++ test.py
@@ -26,3 +26,5 @@ import helper
 helper.convert_coref_ua_to_json('/Users/nicholas/Documents/Dataset/LIGHT/light_dev.2022.CONLLUA', \
                                 '/Users/nicholas/Documents/Dataset/LIGHT/light_dev.2022.bert-base-cased.jsonlines', \
                                 MODEL="coref-hoi", SEGMENT_SIZE=512, TOKENIZER_NAME="bert-base-cased")
+
+# 2023-03-15 目前看起来，数据处理就没什么问题哎。而且其余暂时不用到的部分就先不用看了，否则看起来也是看不完看不动。
